42a2f2152eeb191d093c9e827eb7f96e32e04970
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2022-11-02 13:35:21 +0100

    commented

---
+++ JS/fizzbuzz.js
@@ -0,0 +1,10 @@
+//ciclo for con 100 numeri
+
+//verifico che sia multiplo di 3
+    //stamo fizz
+//verifico che sia multiplo di 5
+    //stampo buzz
+
+//verifico che sia multiplo di 3 e 5
+    //stampo fizzbuzz
+
